9f84c649a376b2661e77fcbc27b3eec698df465f
Habib Akbar Wibowo <hbibakber@gmail.com>
2024-09-19 19:00:56 +0700

    Add scenario on addtocart.feature and add somes object on inventory.page.js

--- features/pageobjects/inventory.page.js
+++ features/pageobjects/inventory.page.js
@@ -8,8 +8,45 @@ class InventoryPage {
     /**
      * define selectors using getter methods
      */
+
+    get inventoryPageUrl() {
+        return 'https://www.saucedemo.com/inventory.html'
+   }
+
     get inventoryPageTitle () {
         return $('//span[text()="Products"]');
+    }
+
+    get btnAddToCart () {
+        return $('//button[@id="add-to-cart-sauce-labs-backpack"]')
+    }
+
+    get btnRemoveProduct () {
+        return $('//button[@id="remove-sauce-labs-backpack"]')
+    }
+
+    get btnCart () {
+        return $('//a[@data-test="shopping-cart-link"]')
+    }
+
+    get cartBadges () {
+        return $('//span[@data-test="shopping-cart-badge"]')
+    }
+
+    get productTest () {
+        return $('//div[@data-test="inventory-item-name" and text() = "Sauce Labs Backpack"]');
+    }
+
+    async assertInventoryUrl() {
+        await expect(browser).toHaveUrl(this.inventoryPageUrl)
+    }
+
+    async assertBadges () {
+        await expect(this.cartBadges).toBeExisting()
+    }
+
+    async assertRemoveBadges () {
+        await expect(this.cartBadges).not.toBeExisting()
     }
 }
 
--- test/specs/addtocart.feature
+++ test/specs/addtocart.feature
@@ -6,3 +6,12 @@ Feature: Add to Cart Functional Tests Saucedemo
         When I add a product to the cart
         Then The product should be added to the cart
         And The cart icon should display badges 1 item
+        
+    Scenario: As a user, I can adding an item to the cart
+        Given I am logged into saucedemo as standard_user
+        And I am on the inventory page
+        When I add a product to the cart
+        And The product should be added to the cart
+        And The cart icon should display badges 1 item
+        And I remove product from the cart
+        Then The cart badges is no longer displayed
